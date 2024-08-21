011e33283ea73eeebfb857876829c937e3952fbb
axartmiami@gmail.com <axartmiami@gmail.com>
2024-08-21 12:15:52 -0400

    implemented homepage with links

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,24 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to IronBeers</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/beers">All Beers</Link>
+          </li>
+          <li>
+            <Link to="/random-beer">Random Beer</Link>
+          </li>
+          <li>
+            <Link to="/new-beer">New Beer</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 export default HomePage;
